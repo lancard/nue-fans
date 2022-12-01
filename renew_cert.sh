8b46e3e298b8f1f41ca17a752b8a9bdcddc0668e
Sungho Kim <worstrocker@gmail.com>
2022-12-01 15:45:05 +0900

    update nuefans to github

--- renew_cert.sh
+++
@@ -1,2 +0,0 @@
-SERVICE_NAME=`grep container_name docker-compose.yml | cut -d: -f 2 | tr -d ' '`
-docker-compose exec -d $SERVICE_NAME certbot -n --nginx -d fans.mywire.org --agree-tos --email worstrocker@gmail.com
